5d9d820883397084f8dca223e23f4d468dad66ab
jlmoer <jlmoerdijk@gmail.com>
2022-01-27 15:10:42 +0100

    State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Box from './components/Box';
+import global from './styles/global.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepsMin = 0;
+const stepsMax = 50000;
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000,
+    }
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <p>Heart : {heartMin}</p>
+          <p>Temperature : {tempMin}</p>
+          <p>Steps : {stepsMin}</p>
+          {/* Water*/}
+          <Box
+            icon="local_drink"
+            color="#3A85FF"
+            value={1.5}
+            unit="L"
+          />
+          {/* Walk */}
+          <Box
+            icon="directions_walk"
+            color="black"
+            value={3000}
+            unit="steps"
+          />
+          {/* Heart */}
+          <Box icon="favorite" color="red" value={120} unit="bpm" />
+          {/* Sun */}
+          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" />
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
